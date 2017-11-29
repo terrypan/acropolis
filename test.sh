87a2ae3e4f0420f8109414690083616e88d77408
Terry Panagoulis <eleftherios.panagoulis.ext@bbc.co.uk>
2017-11-29 16:22:17 +0000

    testing quitl+twine ports

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -ev
-lc=`nc -v localhost`
-acrolc=`nc -v acropolis.localhost`
-echo "localhost says: $lc"
-echo "acropolis.localhost says: $acrolc"
+lc80=`nc -v localhost 80`
+acrolc80=`nc -v acropolis.localhost 80`
+lc8000=`nc -v localhost 8000`
+acrolc8000=`nc -v acropolis.localhost 8000`
+echo "localhost says: $lc80"
+echo "localhost says: $lc8000"
+echo "acropolis.localhost says: $acrolc80"
+echo "acropolis.localhost says: $acrolc8080"
