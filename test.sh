0be89e389dcc4c85d071cff2da5d98358904d154
Terry Panagoulis <eleftherios.panagoulis.ext@bbc.co.uk>
2017-11-29 17:09:12 +0000

    added behave log statement

--- test.sh
+++ test.sh
@@ -4,3 +4,6 @@ lc80=`nc -v localhost 80`
 lc8000=`nc -v localhost 8000`
 echo "localhost says: $lc80"
 echo "localhost says: $lc8000"
+
+rootTTL = `curl -H "Accept: text/turtle" http://localhost`
+echo "root says: $rootTTL"
