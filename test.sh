42a3cd6fce80e7902ccc88fd83ec01d73d633c54
Terry Panagoulis <eleftherios.panagoulis.ext@bbc.co.uk>
2017-11-29 16:47:32 +0000

    test localhost

--- test.sh
+++ test.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -ev
 lc80=`nc -v localhost 80`
-acrolc80=`nc -v acropolis.localhost 80`
 lc8000=`nc -v localhost 8000`
-acrolc8000=`nc -v acropolis.localhost 8000`
 echo "localhost says: $lc80"
 echo "localhost says: $lc8000"
-echo "acropolis.localhost says: $acrolc80"
-echo "acropolis.localhost says: $acrolc8080"
