2d05e78dc9d78d7b2a2e01b0acaf0237ea491bef
Terry Panagoulis <eleftherios.panagoulis.ext@bbc.co.uk>
2017-11-29 17:52:53 +0000

    switched to bash

--- behave-data/run.sh
+++ behave-data/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 sed -i -e "s|ACROPOLIS_HOSTNAME|${ACROPOLIS_HOSTNAME}|" features/config.py
@@ -7,22 +7,22 @@ echo Waiting for services...
 count=20
 until nc -z ${ACROPOLIS_HOSTNAME} 80; do
     echo "$(date) - (${count}) waiting for quilt..."
-		count=$((count-1))
-		if [ "$count" -eq "0" ]; then
-			echo Timeout
-			exit 1;
-		fi
+    count=$((count-1))
+    if [ "$count" -eq "0" ]; then
+      echo Timeout
+      exit 1;
+    fi
     sleep 1
 done
 
 count=15
 until nc -z ${ACROPOLIS_HOSTNAME} 8000; do
     echo "$(date) - (${count}) waiting for twine-remote..."
-		count=$((count-1))
-		if [ "$count" -eq "0" ]; then
-			echo Timeout
-			exit 1;
-		fi
+    count=$((count-1))
+    if [ "$count" -eq "0" ]; then
+      echo Timeout
+      exit 1;
+    fi
     sleep 1
 done
 echo Up!
