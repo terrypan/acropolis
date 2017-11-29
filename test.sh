301c1c4fd07f6b946572772623b1c39be1f02269
Terry Panagoulis <eleftherios.panagoulis.ext@bbc.co.uk>
2017-11-29 17:41:32 +0000

    curl test

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -ev
+set -e
 lc80=`nc -v localhost 80`
 lc8000=`nc -v localhost 8000`
 echo "localhost says: $lc80"
 echo "localhost says: $lc8000"
 
-# rootTTL = `curl -H "Accept: text/turtle" http://localhost`
-# echo "root says: $rootTTL"
+rootTTL=`curl -H "Accept: text/turtle" http://localhost`
+echo "root says: $rootTTL"
